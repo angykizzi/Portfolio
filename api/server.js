f153ffdb533e83f1f87a9fb5d83fa04b9c003c22
angykizzi <angelyescafuller@gmail.com>
2023-05-10 19:34:26 -0400

    aja

--- api/server.js
+++ api/server.js
@@ -6,7 +6,12 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(cors()); // Habilitar CORS
-app.use(express.json());
+const corsOptions = {
+  origin: "https://portfolio-rho-seven-83.vercel.app",
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type",
+};
+app.use(cors(corsOptions));
 app.use("/", router);
 app.use(express.static(path.join(__dirname + "/public")));
 app.listen(5200, () => console.log("Server Running"));
