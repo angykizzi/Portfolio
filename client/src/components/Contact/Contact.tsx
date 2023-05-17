6e3f69cb24b40c438c1d335bfe90baab7a9b514d
angykizzi <angelyescafuller@gmail.com>
2023-05-17 12:56:52 -0400

    probandoemailJS

--- client/src/components/Contact/Contact.tsx
+++ client/src/components/Contact/Contact.tsx
@@ -1,8 +1,9 @@
-import {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import contactImg from '../../assets/img/contact-img.svg'
 import './Contact.css'
 import TrackVisibility from 'react-on-screen';
+import emailjs from '@emailjs/browser';
 
 interface ContactProps {
     category: string;
@@ -17,6 +18,8 @@ interface ContactProps {
   }
 
 export const Contact = () => {
+  const form = useRef<HTMLFormElement | null>(null);
+
     const formInitialDetails = {
         firstName: '',
         lastName: '',
@@ -37,27 +40,25 @@ export const Contact = () => {
         })
     }
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("https://portfolio-production-dbc6.up.railway.app/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setformDetails(formInitialDetails);
-    if (result.code === 200) {
-      setStatus({ success: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ success: false, message: 'Something went wrong, please try again later.'});
-      console.log(result);
-    }
-  };
-
+    const sendEmail = (e: { preventDefault: () => void }) => {
+      e.preventDefault();
+    
+      if (form.current) {
+        emailjs.sendForm(
+          'service_zhslesz',
+          'template_swq67bh',
+          form.current,
+          '4Q2vDHOSU3lgrPwTx'
+        ).then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      }
+    };
 
     return (
         <section className='contact' id='connect'>
@@ -75,30 +76,31 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                     <h2>Get In Touch</h2>
-                    <form onSubmit={handleSubmit}>
+
+                    <form ref={form} onSubmit={sendEmail}>
                         <Row>
                             <Col sm={6} className='px-1'>
-                                <input type='text' value={formDetails.firstName
+                                <input type='text' name="user_name" value={formDetails.firstName
                                 } placeholder='First Name' onChange={(e)=>onFormUpdate({ category: 'firstName', value: e.target.value })}
                                 ></input>
                             </Col>
                             <Col sm={6} className='px-1'>
-                            <input type='text' value={formDetails.lastName
+                            <input type='text' name="user_lastname" value={formDetails.lastName
                                 } placeholder='Last Name' onChange={(e)=>onFormUpdate({ category: 'lastName', value: e.target.value })}
                                 ></input>
                             </Col>
                             <Col sm={6} className='px-1'>
-                            <input type='email' value={formDetails.email
+                            <input type='email' name="user_email"  value={formDetails.email
                                 } placeholder='Email address' onChange={(e)=>onFormUpdate({ category: 'email', value: e.target.value })}
                                 ></input>
                             </Col>
                             <Col sm={6} className='px-1'>
-                            <input type='tel' value={formDetails.phone
+                            <input type='tel' name='user_phone' value={formDetails.phone
                                 } placeholder='Phone No.' onChange={(e)=>onFormUpdate({ category: 'phone', value: e.target.value })}
                                 ></input>
                             </Col>
                             <Col>
-                            <textarea rows={+6} value={formDetails.message} placeholder='Message' onChange={(e)=>onFormUpdate({ category: 'message', value: e.target.value })}></textarea>
+                            <textarea rows={+6} value={formDetails.message} placeholder='Message' name="message" onChange={(e)=>onFormUpdate({ category: 'message', value: e.target.value })}></textarea>
                             <button type="submit" disabled={!isFormValid()}><span>{buttonText}</span></button>
                             </Col>
                             {status.message && 
